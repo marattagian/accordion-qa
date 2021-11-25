a9bfac1a5636ccd8312c9c9f511028438d17f40e
Gianni Maratta <marattagian15@gmail.com>
2021-11-25 17:32:29 -0300

    adding the mock data, some logic to app.jsx and creating a question component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
+import Question from "./components/question"
+import data from "./data"
+
 function App() {
+  const [questions, setQuestions] = useState(data)
 
   return (
-    <div className="box">
+    <div className="container">
+      <h3>Questions and answers about login</h3>
+      <section className="info">
+        {
+          questions.map((question) => <Question key={question.id} />)
+        }
+      </section>
     </div>
   )
 }
---
+++ src/components/question.js
@@ -0,0 +1,3 @@
+function Question() {}
+
+export default Question
